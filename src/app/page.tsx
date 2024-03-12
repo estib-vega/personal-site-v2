c7ce0c11208de0ccd6de3614c24753aaf944e92d
estib <stron@me.com>
2024-03-12 13:33:17 +0100

    Layout: Main content has no padding
    Let the content set the padding if needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Article from "@/components/Article";
 import { ArticleName } from "@/lib/article";
 
 export default function Home() {
-  return <Article article={ArticleName.Portfolio} />;
+  return (
+    <div className="p-4">
+      <Article article={ArticleName.Portfolio} />
+    </div>
+  );
 }
