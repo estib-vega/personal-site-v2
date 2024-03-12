3fff5ee22bd1291965ac3f2eb933ad79ad1b57d9
estib <stron@me.com>
2024-03-12 12:25:14 +0100

    Small style fix: Main content height
    The main content container height extends the whole page as it needs

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -6,7 +6,7 @@ interface MainContentProps {
 
 const MainContent = (props: MainContentProps): JSX.Element => {
   return (
-    <div className="bg-white box-border rounded-xl m-2 p-4 overflow-scroll text-gray-800 shadow-md shadow-blue-600 sm:w-full">
+    <div className="bg-white box-border rounded-xl m-2 p-4 overflow-scroll text-gray-800 shadow-md shadow-blue-600 sm:w-full h-full sm:h-auto">
       {props.children}
     </div>
   );
